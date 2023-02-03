e4012c6d69d9e63118fa91949f2f6f773682045b
joshuepjr <mycoursesonline2021@gmail.com>
2023-02-03 11:33:49 -0500

    Adding a commit without -m parameter

--- 1_data_types/1_first_program.py
+++ 1_data_types/1_first_program.py
@@ -2,4 +2,5 @@
 print("Hello, World, I am Joshuép Jr.!")
 print("I'm here to grow as a software developer!")
 print('Do not stop learing!')
+print('I am updating to commit this code')
 # inside the parentheses of the print() function is a sequence of characters
