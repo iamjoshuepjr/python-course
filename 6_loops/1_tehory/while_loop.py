2f813b8c484a13956cf916635b5e41dc1d4b8548
joshuepjr <mycoursesonline2021@gmail.com>
2023-02-03 22:31:46 -0500

    Sixth commit | I'm really enjoying! <3

---
+++ 6_loops/1_tehory/while_loop.py
@@ -0,0 +1,19 @@
+# ===================================================
+#  A While Loop statement in Python repeatedly
+#  executes a target statements as long as given 
+#  condition is true
+
+counter = 1
+while (counter <= 7):
+    print(counter)
+    counter += 1
+else:
+    print("End While Loop")
+
+z, x = 7, 1
+while z > 0:
+    print(' ' * z + '*' * x + ' ' * z)
+    x += 2
+    z -= 1
+
+
---
+++ 6_loops/2_exercises/square.py
@@ -0,0 +1,7 @@
+# ==================================================
+# Provide a number, print the square of each number 
+
+n = int(input('Enter the name here: '))
+for i in range(n):
+    square = i ** 2
+    print(square)
