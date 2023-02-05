6950b89592c3d88de1f86590c97d76152253f773
joshuepjr <mycoursesonline2021@gmail.com>
2023-02-04 21:02:26 -0500

    This is crazy! :v

---
+++ 7_functions/1_theory/1_function.py
@@ -0,0 +1,132 @@
+# =============================================================
+#  A function is a block of code that performs a specific task
+#  Divding a complex problem into smaller chuncks 
+#  makes our programs easy to understand and reuse
+
+# =================================================================
+#                   Type of functions
+# There are two types of function in Python programming 
+# + Standard library functions
+#   These are built-in functions in Python that are avaible to use 
+# 
+# + User-defined functions
+#   We can create our own functions based on our requirements
+
+# =========================================================
+#       Python Function Declaration
+
+# def -> keyword to declare a function
+# greet -> name given to the function 
+# name -> argument: value passed to function as parameter
+
+print('\n\t\t\t+----------------------+')
+print('\t\t\t-  Intro to functions  -')
+print('\t\t\t+----------------------+\n')
+
+print('\t\t\t+-------------------+')
+print('\t\t\t-  Simple function  -')
+print('\t\t\t+-------------------+\n')
+
+def greet (name):
+    # function body
+    print (f'Hello, {name}!')
+
+# invoking the function
+greet('Joshuép')
+
+print('\n\t\t\t+----------------------+')
+print('\t\t\t-  Function Arguments  -')
+print('\t\t\t+----------------------+\n')
+
+# A funntion can also have arguments. 
+# An argument is a value that is accepted by a function
+
+print('1. Function with argumens: ')
+# function with two arguments
+def add_numbers (x, y):
+    sum = x + y
+    print(f'{x} + {y} = {sum}')
+
+# invoking the function by passing the arguments
+add_numbers(66, 6)
+
+
+
+print('\n\t\t\t+-----------------------+')
+print('\t\t\t-  Function retun Type  -')
+print('\t\t\t+-----------------------+\n\n')
+
+print('\t\t+------------------------+')
+print('\t\t-  Find Square Function  -')
+print('\t\t+------------------------+\n\n')
+
+# function definition
+def find_square(num):    
+    resutl = num * num
+    return resutl
+
+# invoking function
+square = find_square(3)
+
+print(f'Square: {square}')
+
+
+print('\t\t+---------------------------+')
+print('\t\t-  Multiplication Function  -')
+print('\t\t+---------------------------+\n\n')
+
+# function definition 
+def multiplication (x, y):
+    mult = x * y
+    return mult
+
+# invoking function by passing parameters
+print(f'Result: {multiplication(8, 3)} \n\n')
+
+print('\t\t+------------------------------------------+')
+print('\t\t-  Function Arguments with default values  -')
+print('\t\t-------------------------------------------+\n\n')
+
+print(f'Division Function\n')
+
+def division (x = 90, y = 40):
+    result = x / y
+    print(f'Division of {x} / {y} = {result}')
+
+# function call with two arguments
+division(100, 50)
+
+# function call with one argument
+division(x = 69)
+
+# function call with no arguments
+division()
+
+print('')
+print('Python Keyword Argument\n')
+# Keyword arguments are assigned based on the name of arguments
+
+def user(username, password):
+    print(f'Username: {username}')
+    print(f'Password: {password}')
+
+user(username = 'ibatman', password = 'batpassword')
+
+print('\t\t+--------------------------------------+')
+print('\t\t-  Function with Arbitrary Arguments   -')
+print('\t\t---------------------------------------+\n\n')
+
+def find_sum (*numbers):
+    result = 0
+
+    for num in numbers:
+        result += num
+    print(f'Total: {result}')
+
+# invoking function by passing 4 arguments
+find_sum(34, 56, 99, 7)
+
+# invoking function by passing 3 arguments
+find_sum(24, 18, 16)
+print()
+
---
+++ 7_functions/1_theory/4_lambda.py
@@ -0,0 +1,24 @@
+# ========================================================================
+#  Lambda function us a special type of function witout the function name
+
+# Lambda Function Declaration
+# We use the lambda keyword instead of def to create a lambda function
+
+# Syntax lambda arguments : expresions
+
+greet = lambda : print('Hello, Lambda!')
+# defined a lambda function and assigned it to the variable named greet
+# to execute this lambda function, we need to call it
+
+# calling the lambda function
+greet()
+
+
+# lambda function with an argument
+# similar to normal functions, the lambda funtion can also accept arguments
+
+# lambda that accepts one argument
+
+greet_user = lambda name : print(f'Hey there {name}!')
+
+greet_user('Joshuép Jr.')
