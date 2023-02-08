38c96c60049d57f93467d904dc99573d0bd8a5c2
joshuepjr <mycoursesonline2021@gmail.com>
2023-02-08 11:22:01 -0500

    Removing unnecessary lines

--- 8_data_structures/1_lists/2_methods/1_length_method.py
+++ 8_data_structures/1_lists/2_methods/1_length_method.py
@@ -5,23 +5,17 @@
 #  The following code creates a new variable, numberOfPlanet. 
 #  The code assigns that variable with the number of items in the list planets (8)
 
-print()
-print("\t\t\t\t    +---------------+")
-print("\t\t\t\t    - CREATE A LIST -")
-print("\t\t\t\t    +---------------+")
-print()
+print('\n\t\t\t\t    +---------------+')
+print('\t\t\t\t    - CREATE A LIST -')
+print('\t\t\t\t    +---------------+\n')
 
 planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
 print(f'* Planets List: {planets}')
 
-print()
-print("\t\t\t     +---------------------------+")
-print("\t\t\t     -     LENGTH OF A LIST      -")
-print("\t\t\t     +---------------------------+")
-print()
+print('\n\t\t\t     +---------------------------+')
+print('\t\t\t     -     LENGTH OF A LIST      -')
+print('\t\t\t     +---------------------------+\n')
 
 print(f'* Original Planet List: {planets}')
 numberOfPlanets = len(planets)
-print(f'There are {numberOfPlanets} planets in the solar system.')
-print()
-print()
+print(f'There are {numberOfPlanets} planets in the solar system.\n\n')
