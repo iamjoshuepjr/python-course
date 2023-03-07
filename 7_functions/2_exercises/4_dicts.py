c56de850787044316dd87c9c327318bb3dd4cfee
joshuepjr <mycoursesonline2021@gmail.com>
2023-03-07 10:33:59 -0500

    Functions

--- 7_functions/2_exercises/4_dicts.py
+++
@@ -1,9 +0,0 @@
-def displayTerms(**terms):
-    for key, value in terms.items():
-        print(f'{key}:{value}')
-displayTerms(
-    IDE = 'Integrated Development Environment',
-    PK = 'Primary Key'
-)
-
-displayTerms (DBMS = 'Dadabase Management System')
