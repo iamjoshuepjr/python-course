aa48245543e97b8e5adbe739ea84535fec12bcf9
joshuepjr <mycoursesonline2021@gmail.com>
2023-03-12 14:39:33 -0500

    Dictionaries

--- 8_data_structures/4_dictionaries/1_theory/2_iterating_dicts.py
+++ 8_data_structures/4_dictionaries/1_theory/2_iterating_dicts.py
@@ -2,7 +2,15 @@
 #                     Iterating through a dictionary 
 #  We can iterate through each key in a dictionary usin a for loop
 
-week = {1: 'Sunday', 2: 'Monday', 3: 'Tuesday', 4: 'Wednesday', 5: 'Thursday', 6: 'Friday', 7: 'Saturday'}
+week = {
+    1: 'Sunday', 
+    2: 'Monday', 
+    3: 'Tuesday', 
+    4: 'Wednesday', 
+    5: 'Thursday', 
+    6: 'Friday', 
+    7: 'Saturday'
+}
 
 print('\n------------------------------')
 print('1. Iterating key Dictionaries: ')
@@ -10,9 +18,8 @@ print('------------------------------')
 print('* Days of the week:   ')
 for i in week:
     print(week[i])
-print('------------------------------')
+print('\n------------------------------')
 print('* Term and value:   ')
 for term, value in week.items():
     print(f'Key: {term} - Value: {value}')
 print('------------------------------')
-
