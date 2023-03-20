2c88bf73eebc5da1a0ad76a7750276dade0c046f
joshuepjr <mycoursesonline2021@gmail.com>
2023-03-19 19:52:56 -0500

    Modules and Packages

---
+++ 9_modules_and_packages/exercices/lambda_functions/arithmetic.py
@@ -0,0 +1,11 @@
+# Arithmetics Operations Module
+
+addition = lambda a, b: a + b
+subtract = lambda a, b: a - b
+multiply = lambda a, b: a * b
+# division = lambda a, b: a / b
+def division(a, b):
+    if(b == 0):
+        return 0
+    else:
+        return a / b
