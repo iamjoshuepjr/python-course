2754227a7c9b3764f32089d55e4b639e893f114d
joshuepjr <mycoursesonline2021@gmail.com>
2023-04-14 11:43:50 -0500

    Python Fundamentals

---
+++ 1_fundamentals/1_data_types/1_first_program.py
@@ -0,0 +1,6 @@
+# print() is a function that tells the computer to perform an action
+print("Hello, World, I am Joshuép Jr.!")
+print("I'm here to grow as a software developer!")
+print('Do not stop learing!')
+print('I am updating to commit this code')
+# inside the parentheses of the print() function is a sequence of characters
