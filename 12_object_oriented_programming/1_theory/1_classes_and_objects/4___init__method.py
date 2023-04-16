ae45c2440dfc30d60b546f7124be0c4c251ebaa6
joshuepjr <mycoursesonline2021@gmail.com>
2023-04-16 18:42:15 -0500

    Python-OOP

---
+++ 12_object_oriented_programming/1_theory/1_classes_and_objects/4___init__method.py
@@ -0,0 +1,3 @@
+# When you create a new object of a class, 
+# Python automatically calls the __init__ method to initialize the object's attributes.
+# Unlike regular methods, the __init__ method has two underscores (__)
