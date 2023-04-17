3270b6d770bc724e9e7c0846dc919cdd7310d13f
joshuepjr <mycoursesonline2021@gmail.com>
2023-04-16 20:10:25 -0500

    Python-OOP

--- 12_object_oriented_programming/1_theory/1_classes_and_objects/2_class_variables.py
+++ 12_object_oriented_programming/1_theory/1_classes_and_objects/2_class_variables.py
@@ -1,18 +1,18 @@
-# Everything in python is an object including a class.
-# When you define a clas using the clas keyword, Python creates an object 
-# with the same name as the class's name.
+# In Object-Oriented Programming, a class variable (also known as a static variable) 
+# is a variable that belongs to a class rather than an instance of that class. 
+# Class variables are variables that are defined within a class but outside of any method or constructor. 
+# They are shared by all instances (objects) of that class and can be accessed via the class name or any instance of the class.
 
 from pprint import pprint
 
 class Dog():
-    # this sentence defines the Person class and the Person object. 
-    # The Person object has the __name__ property.
-        
-    # define class attributes
+            
     # Class attributes are bound to the class. They are shared by all instances of that class.
-    # They are helpful if you want to define class constants or variables that keep track 
-    # of the number of instances of a class.
+    # They are helpful if you want to define class constants or variables that keep track of the number of instances of a class.
 
+    # To define class attributes, you simply declare it within the class body.
+    species = 'Canis familiaris'
+    legs = 4
     sound = 'Woof' # Class attribute
     counter = 0
 
@@ -22,16 +22,16 @@ class Dog():
 # dot notation (class.attribute)
 
 print(f'\n(Class Attribute accessed by class)\
-    \nDog Counter: {Dog.counter}\
-    \nDog Sound: {Dog.sound}!')
+        \nDog Counter: {Dog.counter}\
+        \nDog Sound: {Dog.sound}!')
 
 # Another way to get the value of a class varibale is to use the getattr() function. 
 # The getattr() function accepts an object and a variable name. 
 # It returns the value of the class variable.
 
 dog_sound = getattr(Dog, 'sound')
-print(f'\n(Class Attribute accessed by class with getattr()\nDog Sound: {dog_sound}!')
-
+print(f'\n(Class Attribute accessed by class with getattr()\
+        \nDog Sound: {dog_sound}!')
 
 # Set values for class variables
 # To set a value for a class variables, you use the dot notation (ClassName.attribute)
@@ -41,14 +41,13 @@ Dog.sound = 'Meow'
 setattr(Dog, 'counter', 5)
 
 print(f'\n(Class Attribute setting new values:)\
-    \nDog Sound: {Dog.sound}!\
-    \nDog Counter: {Dog.counter}') 
+        \nDog Sound: {Dog.sound}!\
+        \nDog Counter: {Dog.counter}') 
 
 # Class variable storage
 # Python store class variables in the __dict__ attribute. 
 # The __dict__ attribute is a mapping proxy, which is a dictionary 
 pprint(f'Dog Varible Storage: {Dog.__dict__}')
-
 
 # Delete class variables
 # To delete a class variable at runtime, you use the delattr() function
@@ -56,11 +55,3 @@ pprint(f'Dog Varible Storage: {Dog.__dict__}')
 # delattr(Dog, 'counter') # AttributeError: type object 'Dog' has no attribute 'counter'
 print(f'(Delete class attributes)\
      \nDog Counter: {Dog.counter}')
-
-
-# Desmostrating classes are objetct too
-print(f'\nClasses are objects too!\nObject Name Attribute: {Dog.__name__}')
-print(f'Object Class: {type(Dog)}')
-
-# It's an instance of the type class
-print(f'Is the object {Dog.__name__} an instance of {type.__name__}? {isinstance(Dog, type)}')
