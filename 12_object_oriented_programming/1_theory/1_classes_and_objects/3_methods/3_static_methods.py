a197686a4a0c3e7b9f5ae73a6168e5738e5fe06c
joshuepjr <mycoursesonline2021@gmail.com>
2023-04-19 21:35:01 -0500

    Python-OOP

--- 12_object_oriented_programming/1_theory/1_classes_and_objects/3_methods/3_static_methods.py
+++ 12_object_oriented_programming/1_theory/1_classes_and_objects/3_methods/3_static_methods.py
@@ -88,3 +88,46 @@ class Test:
 
 # call class method
 Test.class_method_1()
+
+class TemperatureConverter:
+    KELVIN = 'K'
+    FAHRENHEIT = 'F'
+    CELCIUS = 'C'
+
+    @staticmethod
+    def celcius_to_fahrenheit(c):
+        return 9 * c/5 + 32
+    
+    @staticmethod
+    def fahrenheit_to_celcius(f):
+        return 5 * (f - 32)/9
+
+    @staticmethod
+    def celcius_to_kelvin(c):
+        return c + 273.15
+    
+    @staticmethod
+    def kelvin_to_celcius(k):
+        return k - 273.15
+
+    @staticmethod
+    def fahrenheit_to_kelvin(f):
+        return 5 * (f + 459.67)/9
+    
+    @staticmethod
+    def kelvin_to_fahrenheit(k):
+        return 9 * k/5 - 459.67
+    
+    @staticmethod
+    def format(value, unit):
+        symbol = ''
+        if unit == TemperatureConverter.FAHRENHEIT:
+            symbol = '°F'
+        elif unit == TemperatureConverter.CELCIUS:
+            symbol = '°C'
+        elif unit == TemperatureConverter.KELVIN:
+            symbol = '°K'
+        
+        return f'{value}{symbol}'
+f = TemperatureConverter.celcius_to_fahrenheit(35)
+print(TemperatureConverter.format(f, TemperatureConverter.FAHRENHEIT))
