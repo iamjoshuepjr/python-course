87445401459089126a20c7fb9771ededf190761b
joshuepjr <mycoursesonline2021@gmail.com>
2023-04-23 16:40:37 -0500

    Rectangle

--- 12_object_oriented_programming/2_exercises/Rectangle.py
+++ 12_object_oriented_programming/2_exercises/Rectangle.py
@@ -12,19 +12,26 @@ class Rectangle:
     
     def calculate_area(self):
         ''' 
-        Calculate the are of the rectangle.
+        Calculate the area of the rectangle.
         Returns:
-            int: The are of the rectangle.
+            int: The area of the rectangle.
         '''
         return self.base * self.height
+    
+    def calculate_perimeter(self):
+        ''' 
+        Calculate the perimeter of the rectangle.
+        Returns:
+            int: The perimeter of the rectangle.
+        '''
+        return (self.height * 2) + (self.base * 2)
 
     def ask_values(self):
         ''' Ask the user to input new values for the base and height of the rectangle. '''
         self.base = int(input('Ente the base: '))
         self.height = int(input('Ente the height: '))
 
-
-
 rectangle = Rectangle(0, 0) # create an instance with initial values of 0
 rectangle.ask_values() # ask the user to input new values
 print(f'Area: {rectangle.calculate_area()}')
+print(f'Perimeter: {rectangle.calculate_perimeter()}')
