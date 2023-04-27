7d05145adf16c49280b02704f8691aeb4f72c079
joshuepjr <mycoursesonline2021@gmail.com>
2023-04-27 12:38:40 -0500

    OOP-Projects

--- 2_Object_Oriented_Programming/3_projects/PC/pc_test.py
+++ 2_Object_Oriented_Programming/3_projects/PC/pc_test.py
@@ -14,7 +14,13 @@ mouse2  = Mouse('Samsung', 'Bluetooth')
 monitor2 = Monitor('Acer', 34)
 computer2 = Computer('HP', monitor2, keyboard2, mouse2)
 
+keyboard3 = Keyboard('Gamer', 'Bluetooth')
+mouse3 = Mouse('Gamer', 'Bluetooth')
+monitor3 = Monitor('Gamer', 45)
+computer3 = Computer('Gamer', keyboard3, mouse3, monitor3)
+
 computer_list = [computer, computer2]
 
 order1 = Order(computer_list)
+order1.add_computer(computer3)
 print(order1)
