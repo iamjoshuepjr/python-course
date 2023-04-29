fd4900c8ae5adcf5fcb207d239f5f5783c82fafb
joshuepjr <mycoursesonline2021@gmail.com>
2023-04-28 19:23:16 -0500

    File-Handling

---
+++ 1_Fundamentals/12_file_handling/1_theory/4_write_files/1_write.py
@@ -0,0 +1,54 @@
+"""
+ ===============================================================
+                 Write file 
+ =============================================================== 
+ We can open a file and do different operation on it, such as 
+ write new content into it or modify a file by appending content 
+ at the end of a file. 
+ ===============================================================
+   File mode                Meaning 
+ =============================================================== 
+ w            Open a file for writing. In the case if an 
+              existing file, it truncantes the existing content 
+              and places the filhandle at the beginning of the file.
+              A new file is created if the mentioned file doesn't exist. 
+              
+ w+           Open a file for both reading and writing. 
+              In the case of the exiting file, it will truncate 
+              the entire content and place the filehandle at the
+              beginning of the file.
+ 
+ wb           Open a binary file for writing. The filehadle is placed 
+              at the beginning of the file, and existing content 
+              will be truncated. A new file is created otherwise.              
+
+ a            Opens file for writing. The filehadle is placed 
+              at the end of the file. In the case of existing file, the 
+              new content will be added after the existing content. 
+              A new file is created otherwise.              
+
+ a+           Opens file for appending as well as reading. 
+              The filehadle is placed at the end of the file. 
+              In the case of existing file, the new content will 
+              get added after the existing content. 
+              A new file is created otherwise.              
+"""
+text = "This is to be written"
+file = open('1_Fundamentals\\12_file_handling\\1_theory\\1_create_files\\files\\2_write\\7_write.txt', 'w')
+file.write(text)
+print('Done writing')
+file.close()
+
+file = open('1_Fundamentals\\12_file_handling\\1_theory\\1_create_files\\files\\2_write\\7_write.txt', 'r')
+print(file.read())
+file.close()
+
+text = "This is new content to be written"
+file = open('1_Fundamentals\\12_file_handling\\1_theory\\1_create_files\\files\\2_write\\7_write.txt', 'w+')
+file.write(text)
+print('Done writing')
+file.close()
+
+file = open('1_Fundamentals\\12_file_handling\\1_theory\\1_create_files\\files\\2_write\\7_write.txt', 'r')
+print(file.read())
+file.close()
---
+++ 1_Fundamentals/12_file_handling/1_theory/4_write_files/2_writelines.py
@@ -0,0 +1,50 @@
+""" 
+ =====================================================================
+        writelines()
+ =====================================================================
+ We can write a multiple lines at once using the writelines().
+ We can pass a list of strings that we want to add to the file to it.
+ Use this method when you wanted to write a list into a file.
+"""
+
+person_data = ['Name: Emma', '\nAddress: 221 Baker Street', '\nCity: London']
+# writing string and list of lines to a file
+fp = open('1_Fundamentals\\12_file_handling\\1_theory\\1_create_files\\files\\2_write\\8_write.txt', 'w')
+fp.writelines(person_data)
+fp.close()
+
+# opening the file in read mode
+fp = open('1_Fundamentals\\12_file_handling\\1_theory\\1_create_files\\files\\2_write\\8_write.txt', 'r')
+print(fp.read())
+fp.close()
+
+# 
+name = "Written using a context manager"
+with open('1_Fundamentals\\12_file_handling\\1_theory\\1_create_files\\files\\2_write\\9_write.txt', 'w') as f:
+    f.write(name)
+
+# opening the file in read mode to access the file
+with open('1_Fundamentals\\12_file_handling\\1_theory\\1_create_files\\files\\2_write\\9_write.txt', 'r') as f:
+    print(f.read())
+
+# Appending new content to an existing file
+name = '\nName: Ava'
+address = ['\nAddress: 221 Baker Street', '\nCity: London', '\nCountry:United Kingdom']
+# append to file
+with open('1_Fundamentals\\12_file_handling\\1_theory\\1_create_files\\files\\2_write\\8_write.txt', 'a') as f:
+    f.write(name)
+    f.writelines(address)
+
+# opening the file in read mode to access the file
+with open('1_Fundamentals\\12_file_handling\\1_theory\\1_create_files\\files\\2_write\\8_write.txt', 'a+') as f:
+    print(f.read())
+
+name = '\nName: Antony'
+address = ['\nAddress: 221 Baker Street', '\nCity: London', '\nCountry:United Kingdom']
+# append to file
+with open('1_Fundamentals\\12_file_handling\\1_theory\\1_create_files\\files\\2_write\\8_write.txt', 'a+') as f:
+    f.write(name)
+    f.writelines(address)
+    # move file handle to the start
+    f.seek(0)
+    print(f.read())
