c0e8402b02b26a3bdfc110a89b36376d87e64f89
joshuepjr <mycoursesonline2021@gmail.com>
2023-05-02 21:06:51 -0500

    DateTime

--- 3_DateTime/1_current_datetime/1_current_datetime.py
+++ 3_DateTime/1_current_datetime/1_current_datetime.py
@@ -7,7 +7,8 @@
 # ===================
 #  current datatime
 # ===================
-from datetime import datetime
+from datetime import datetime, timezone, date 
+import time
 
 now = datetime.now()
 print(f'\nCurrent datetime: {now}\nType: {type(now)}')
@@ -31,6 +32,65 @@ print(f'+-------------------+\
       \n+-------------------+\
       \n Hour: {now.hour}\
       \n Minute: {now.minute}\
-      \n Second: {now.second}\
-      \n Microsecond: {now.microsecond}\
-      \n+-------------------+')
+      \n Seconds: {now.second}\
+      \n Microseconds: {now.microsecond}\
+      \n+-------------------+') 
+
+# =========================================
+#  get current date using the date class
+# =========================================
+today = date.today()
+print(f'\n+------------------------------------+\
+        \n* Current date: {today}')
+
+# ==============================================
+#  get current time in seconds with time class
+# ==============================================
+seconds = time.time()
+print(f'* Time in seconds {seconds}')        
+
+# =========================================
+#  get current date using the time.ctime()
+# =========================================
+print(f'* Current Time: {time.ctime(time.time())}')
+
+# =============================================
+#  get current date using the time.localtime()
+# =============================================
+local = time.localtime(time.time())
+
+print(f'+-------------------+\
+      \n Year: {local.tm_year}\
+      \n Month: {local.tm_mon}\
+      \n Day: {local.tm_mday}\
+      \n+-------------------+\
+      \n Hour: {local.tm_hour}\
+      \n Minute: {local.tm_min}\
+      \n Seconds: {local.tm_sec}\
+      \n+-------------------+') 
+
+# ====================================
+#  get current time in milliseconds
+# ====================================
+miliseconds = time.time()
+miliseconds = int(miliseconds * 1000)
+print(f'Miliseconds: {miliseconds}')
+
+# =============================================
+#  get current Coordinate Universal Time (UTC) 
+# =============================================
+now = datetime.now(timezone.utc)
+print(f'Current UTC Time: {now}')
+
+# ===========================================
+#  get current Greenwich Mean Time (GMT) 
+# ===========================================
+gmt = time.gmtime(time.time())
+print(f'GMT Time: {gmt}')
+print(f'{gmt.tm_hour}: {gmt.tm_min}: {gmt.tm_sec}')
+
+# ===================================
+#  get current time in ISO format 
+# ===================================
+dt_iso = datetime.now().isoformat()
+print(f'Current DateTime in ISO: {dt_iso}')
---
+++ 3_DateTime/1_current_datetime/2_datetime_format.py
@@ -0,0 +1,51 @@
+# ====================================================================== 
+#                  DateTime Format (Strftime)
+# ====================================================================== 
+# In Python, the date and time values are stored as datetime objects, 
+# but there are cases where we need to print the datetime objects into 
+# various string formats for better readability.
+# For example, you may need to represent a date numerically in format, 
+# like “17-06-2021“. On the other hand, you want to convert dates in 
+# textual string format like “Tuesday, 23 June 2021.”
+
+from datetime import datetime
+
+# current datetime
+now = datetime.now()
+
+# convert to string
+date_time = now.strftime('%Y-%m-%d-%H:%M:%S')
+print(f'DateTime String: {date_time}')
+
+# ======================================================================
+# Convert individual attribuyes of a datetime object to a string format
+# ======================================================================
+
+date = now.strftime('%Y-%m-%d')
+print(f'Date String: {date}')
+
+time = now.strftime('%H:%M:%S')
+print(f'Time String: {time}')
+
+year = now.strftime('%Y')
+print(f'Year String: {year}')
+
+month = now.strftime('%m')
+print(f'Month String: {month}')
+
+day = now.strftime('%d')
+print(f'Day String: {day}')
+
+# ====================================
+#  Represent Dates in Textual Format 
+# ====================================
+# Full Textual Mode
+print(f'dd-MonthName-yyyy: {now.strftime("%d-%B-%Y")}')
+print(f'DayName-MonthName-yyyy: {now.strftime("%A, %d - %B, %Y")}')
+
+# Short Textual Mode
+print(f'dd-MonthName-yyyy: {now.strftime("%d-%b-%Y")}')
+print(f'DDD-dd-MMM-yyyy: {now.strftime("%a, %d - %b, %Y")}')
+
+
+
