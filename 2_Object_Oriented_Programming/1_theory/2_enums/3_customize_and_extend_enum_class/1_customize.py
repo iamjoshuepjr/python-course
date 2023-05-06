5eb8dd73998360bdca0460c5b54598795c62f246
joshuepjr <mycoursesonline2021@gmail.com>
2023-05-06 08:28:54 -0500

    Enumeration

--- 2_Object_Oriented_Programming/1_theory/2_enums/3_customize_and_extend_enum_class/1_customize.py
+++ 2_Object_Oriented_Programming/1_theory/2_enums/3_customize_and_extend_enum_class/1_customize.py
@@ -1,5 +1,7 @@
 from enum import Enum 
+from functools import total_ordering
 
+@total_ordering
 class PaymentSatus(Enum):
     PENDING = 1
     COMPLETED = 2
@@ -23,8 +25,35 @@ class PaymentSatus(Enum):
             return self is other 
         
         return False 
+    
+    # implementing __lt__ method 
+    # To allow the sorting base on their value 
 
-print(PaymentSatus.PENDING)
+    def __lt__(self, other):
+        if isinstance(other, int):
+            return self.value < other
+        
+        if isinstance(other, PaymentSatus):
+            return self.value <  other.value
+        
+        return False
+    
+    # implementing __bool__ method
+    def __bool__(self):
+        if self is self.COMPLETED:
+            return True
+        
+        return False
 
-if(PaymentSatus.PENDING == 1):
-    print('The payment is pending.')
+# compare with an integer
+status = 1
+if status < PaymentSatus.COMPLETED:
+    print('The payment has not completed!')
+
+# compare with another member
+status = PaymentSatus.PENDING
+if status >= PaymentSatus.COMPLETED:
+    print('The payment is not pending.')
+
+for member in PaymentSatus:
+    print(member, bool(member))
