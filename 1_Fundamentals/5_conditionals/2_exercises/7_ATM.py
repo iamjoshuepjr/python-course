1468bc38bda225c707b6d4bf7f63a748db4308a6
iamjoshuepjr <mycoursesonline2021@gmail.com>
2023-10-15 10:58:50 -0500

    Fundamentals - Conditionals

--- 1_Fundamentals/5_conditionals/2_exercises/7_ATM.py
+++ 1_Fundamentals/5_conditionals/2_exercises/7_ATM.py
@@ -1,48 +1,75 @@
+import os
+
+def clear_screen():
+    os.system('clear')
+
+
+def leave_app():
+    exit_app = input('Do you want to leave the app?\nYour response: ')
+
 balance = 10000
 deposit, withdraw, currentBalance = 0, 0, 0
 
-print('+------------------+')
-print('-  Welcome to ATM  -')
-print('+------------------+')
-print('- Select an option -') 
-print('- 1. Accoutn       -') 
-print('- 2. Deposit       -') 
-print('- 3. Withdraw      -') 
-print('- 4. Exit          -')
-print('+------------------+') 
-      
-choice = int(input('Enter your choice here: '))
 
-match choice:
-    case 1:
-        print('+----------------------------+')
-        print(f'- Your Balance is $US {balance}  -')
-        print('+----------------------------+')
-    case 2:
-        deposit = float(input('Enter the deposit: '))
-        currentBalance = balance + deposit
-        print('\n+--------------------+')
-        print('- Saved Successfully -')
-        print('+--------------------+')
-    case 3: 
-        withdraw = float(input('Enter the amount to withdraw: '))
-        if(withdraw >  balance):
-            print('\n+-------------------------------------------------+')
-            print('- Invalid Action!\n- The amount to withdraw is greater thab balance. -')
-            print('+-------------------------------------------------+')
-        else:
-            currentBalance = balance - withdraw
-            print('+-------------------------------+')
-            print(f'- New Balance: $US {currentBalance} -')
-            print(f'- Withdraw: $US {withdraw} - ')
-            print('+-------------------------------+')
-    case 4:
-        print('+-----------------------------------+')
-        print('- Thank you for using our services! -')
-        print('+-----------------------------------+')
+exit_app = 'n'
+while exit_app == 'n':
+    print('+------------------+')
+    print('-  Welcome to ATM  -')
+    print('+------------------+')
+    print('- Select an option -') 
+    print('- 1. Account       -') 
+    print('- 2. Deposit       -') 
+    print('- 3. Withdraw      -') 
+    print('- 4. Exit          -')
+    print('+------------------+') 
+      
+    choice = int(input('Enter your choice here: '))
+
+    match choice:
+        case 1:
+            clear_screen()
+            print('+----------------------------+')
+            print(f'- Your Balance is $US {balance}  -')
+            print('+----------------------------+')
+            leave_app()
+            clear_screen()
+        case 2:
+            clear_screen()
+            deposit = float(input('Enter the deposit: '))
+            currentBalance = balance + deposit
+            print('\n+--------------------+')
+            print('- Saved Successfully -')
+            print('+--------------------+')
+            leave_app()
+            clear_screen()
+        case 3: 
+            clear_screen()
+            withdraw = float(input('Enter the amount to withdraw: '))
+            if(withdraw >  balance):
+                print('\n+-------------------------------------------------+')
+                print('- Invalid Action!\n- The amount to withdraw is greater thab balance. -')
+                print('+-------------------------------------------------+')
+            else:
+                currentBalance = balance - withdraw
+                print('+-------------------------------+')
+                print(f'- New Balance: $US {currentBalance} -')
+                print(f'- Withdraw: $US {withdraw} - ')
+                print('+-------------------------------+')
+                leave_app()
+                clear_screen()
+        case 4:
+            clear_screen()
+            print('+-----------------------------------+')
+            print('- Thank you for using our services! -')
+            print('+-----------------------------------+')
+            leave_app()
+            clear_screen()
         
-    case _:
-        print('+-------------------+')
-        print('-     Warning!      -')
-        print('- Invalid choice!   -')
-        print('+-------------------+')
+        case _:
+            clear_screen()
+            print('+-------------------+')
+            print('-     Warning!      -')
+            print('- Invalid choice!   -')
+            print('+-------------------+')
+            leave_app()
+            clear_screen()
