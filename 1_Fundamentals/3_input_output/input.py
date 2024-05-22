a230e1f68dbe5033b1e6fa56a6c349239209ecca
iamjoshuepjr <mycoursesonline2021@gmail.com>
2024-05-21 21:17:20 -0500

    Fundamentals - Input

--- 1_Fundamentals/3_input_output/input.py
+++ 1_Fundamentals/3_input_output/input.py
@@ -1,24 +1,27 @@
 # Input - Output  
   
-
 # input function -> allow us get from user by standar input values to process
-# this methos only get str
+# this methods only get str
 name = input("Enter your name here: ")
 age = int(input("Enter your age here: "))
 birthday = input("Enter your birthday (dd/mm/yyyy): ")
 ip = input("Enter your IP here: ")
-print()
-print("\t+-----------------------+")
+height = float(input('Enter your hight in meters: '))
+
+print("\n\t+-----------------------+")
 print(f"\t Name {name}")
 print(f"\t Age: {age}")
 print(f"\t Birthday: {birthday}")
+print(f"\t Height: {height} meters")
 print(f"\t IP: {ip}")
-print("\t+------------------------+")
+print("\t+------------------------+\n")
 
-print()
 print("+----------------------------+")
-print(" [Good - Regular - Bad]      ")
+print(""" 
+    Provide a feedback about your day
+      + Good
+      + Regular
+      + Bad """)
 result = input(" How was your day?  ")
 print(f" Your day was: {result}     ")
-print("+----------------------------+")
-print()
+print("+----------------------------+\n")
