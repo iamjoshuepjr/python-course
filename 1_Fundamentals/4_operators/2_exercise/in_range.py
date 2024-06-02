79a40789d08eff3ddd32f47d6a7b691346a7bf91
iamjoshuepjr <mycoursesonline2021@gmail.com>
2024-06-02 00:43:19 -0500

    Fundamentals - Operators Exercises

--- 1_Fundamentals/4_operators/2_exercise/in_range.py
+++ 1_Fundamentals/4_operators/2_exercise/in_range.py
@@ -1,4 +1,3 @@
-print()
 value = int(input("Enter a value between 0 - 5 here: "))
 
 minValue = 0
@@ -7,5 +6,4 @@ maxValue = 5
 result = (value >= minValue) and (value <= maxValue)
 print(f'In range is: {result}. So {value} is in range {minValue} and {maxValue}')
 result = (value <= minValue) and (value >= maxValue)
-print(f'Out of range is: {result}. So {value} is in range {minValue} and {maxValue}')
-print()
+print(f'Out of range is: {result}. So {value} is in range {minValue} and {maxValue}\n')
