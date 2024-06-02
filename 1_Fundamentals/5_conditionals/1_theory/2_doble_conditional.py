c30c71eab24cc0180c61d5db05aab9fb5458c7b3
iamjoshuepjr <mycoursesonline2021@gmail.com>
2024-06-02 00:57:32 -0500

    Fundamentals - Operators Exercises

--- 1_Fundamentals/5_conditionals/1_theory/2_doble_conditional.py
+++ 1_Fundamentals/5_conditionals/1_theory/2_doble_conditional.py
@@ -3,15 +3,12 @@
 #  This control structure allows us make-decitions depending the evaluation result:
 #  There are tow possibilities (if - else) and the condition determines which one gets executed
 
-
-print()
-age = int(input("Enter your age here, please: "))
+age = int(input("\nEnter your age here, please: "))
 if (age > 18):
-    print("+---------------------------------+")
-    print("- Congrats! You're of legal age!  -")
-    print("+---------------------------------+")
+    print("+--------------------------------+")
+    print("  Congrats! You're of legal age!  ")
+    print("+--------------------------------+")
 else:
-    print("+----------------------------------+")
-    print("- Sorry!\nYou aren't of legal age! -")
-    print("+----------------------------------+")
-print()
+    print("+-------------------------+")
+    print(" Sorry!\n You aren't of legal age! ")
+    print("+-------------------------+\n")
--- 1_Fundamentals/5_conditionals/1_theory/3_chained_conditionals.py
+++ 1_Fundamentals/5_conditionals/1_theory/3_chained_conditionals.py
@@ -3,16 +3,11 @@
 #  One way to express a computation like that is a chained conditional
 #  elif - is an abbreviation of "else-if"
 
-print()
-age = int(input('Enter your age here, please: '))
+age = int(input('\nEnter your age here, please: '))
 
 if(age < 0):
-    print()
-    print('Warning!\nYour age must not be a negative number!')
+    print('\nWarning!\nYour age must not be a negative number!\n')
 elif (age < 18):
-    print()
-    print('Warning!\nYou are not of legal age')
+    print('\nWarning!\nYou are not of legal age.\n')
 else:
-    print()
-    print('Congrats, you are of legal age')
-print()
+    print('\nCongrats, you are of legal age\n')
